87f84ee4d78a0be0bec8180473276ca55422df14
Your Name <you@example.com>
2022-08-16 15:11:26 +0200

    zip and unzip data file

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,44 @@
 "use strict";
 exports.__esModule = true;
 var index_1 = require("../node_modules/axios/index");
+var CronJob = require('cron').CronJob;
+var fs = require("fs");
 var URL = {
-    value: "https://data.assemblee-nationale.fr/static/openData/repository/16/vp/syceronbrut/syseron.xml.zip"
+    value: process.env.SOURCE_FILE
 };
-index_1["default"].get(URL.value)
-    .then(function (response) {
-    console.log(response);
-})["catch"](function (error) {
-    console.log(error);
+var LOCALFILE = {
+    input: "./tmp/publicDebateReports.zip",
+    output: "./tmp/unzippedPublicDebateReports.xml"
+};
+var downloadZipFile = new Promise(function (resolve, reject) {
+    (0, index_1["default"])({ url: URL.value, method: 'get', responseType: 'stream' })
+        .then(function (response) {
+        resolve(response.data);
+    })["catch"](function (error) {
+        reject(error);
+    });
+});
+var job = new CronJob('0 0 * * *', function () {
+}, null, true, 'Europe/Paris');
+var zlib = require('zlib');
+var yauzl = require("yauzl");
+yauzl.open(LOCALFILE.input, { lazyEntries: true }, function (err, zipfile) {
+    if (err)
+        throw err;
+    zipfile.readEntry();
+    zipfile.on("entry", function (entry) {
+        if (/\/$/.test(entry.fileName)) {
+            zipfile.readEntry();
+        }
+        else {
+            zipfile.openReadStream(entry, function (err, readStream) {
+                if (err)
+                    throw err;
+                readStream.on("end", function () {
+                    zipfile.readEntry();
+                });
+                readStream.pipe(fs.createWriteStream(LOCALFILE.output));
+            });
+        }
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,72 @@
 import axios from "../node_modules/axios/index";
+const CronJob = require('cron').CronJob;
+const fs = require("fs");
 
 interface Url {
-    value: string;
+  value: string;
 }
 
-const URL : Url = {
-    value: "https://data.assemblee-nationale.fr/static/openData/repository/16/vp/syceronbrut/syseron.xml.zip"
+interface localFile {
+  input: string,
+  output: string
+}
+
+const URL: Url = {
+  value: process.env.SOURCE_FILE,
 } 
 
-axios.get(URL.value)
+const LOCALFILE: localFile = {
+  input: "./tmp/publicDebateReports.zip",
+  output: "./tmp/unzippedPublicDebateReports.xml"
+}
+
+const downloadZipFile = new Promise((resolve, reject) => {
+  axios({url: URL.value, method: 'get', responseType: 'stream'})
   .then(function (response) {
-    // handle success
-    console.log(response);
+    resolve(response.data)
   })
   .catch(function (error) {
-    // handle error
-    console.log(error);
+    reject(error)
   })
+});
+
+/* downloadZipFile
+  .then((data: any)=>{
+    data.pipe(fs.createWriteStream(LOCALFILE.input));  
+  }); */
+
+let job = new CronJob(
+  '0 0 * * *', 
+  function() {
+    // run task here
+  },
+  null,
+  true,
+  'Europe/Paris'
+  );
+
+const zlib = require('zlib');
+
+var yauzl = require("yauzl");
+
+yauzl.open(LOCALFILE.input, {lazyEntries: true}, function(err:any, zipfile:any) {
+  if (err) throw err;
+  zipfile.readEntry();
+  zipfile.on("entry", function(entry:any) {
+    if (/\/$/.test(entry.fileName)) {
+      // Directory file names end with '/'.
+      // Note that entries for directories themselves are optional.
+      // An entry's fileName implicitly requires its parent directories to exist.
+      zipfile.readEntry();
+    } else {
+      // file entry
+      zipfile.openReadStream(entry, function(err:any, readStream:any) {
+        if (err) throw err;
+        readStream.on("end", function() {
+          zipfile.readEntry();
+        });
+        readStream.pipe(fs.createWriteStream(LOCALFILE.output));
+      });
+    }
+  });
+});
