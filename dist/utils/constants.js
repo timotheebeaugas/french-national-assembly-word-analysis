60f7541f87b48b149e5a044ae6e77ed0cf324331
Your Name <you@example.com>
2022-12-16 17:59:02 +0100

    url /utils

--- dist/utils/constants.js
+++ dist/utils/constants.js
@@ -1,6 +1,6 @@
-export const SOURCE_FILE_URL = process.env.SOURCE_FILE_URL;
 export const LOCAL_FILES_PATHS = {
     input: "./tmp/",
     output: "./tmp/",
 };
 export const FETCH_URL_INTERVAL = "0 0 * * *";
+export const REMOTE_DATA_SOURCE_URL = "https://assemblee-nationale.fr/";
