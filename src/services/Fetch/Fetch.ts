44def97b9a705cf014fe34f8f47cdd59c6c4b4f8
Your Name <you@example.com>
2023-02-13 19:04:55 +0100

    fix somes issues from Fetch method

--- src/services/Fetch/Fetch.ts
+++ src/services/Fetch/Fetch.ts
@@ -1,55 +1,45 @@
 import axios from "axios";
+import * as fs from "fs";
+import * as path from "path";
 import {
   LOCAL_FILES_PATHS,
   REMOTE_DATA_SOURCE_URL,
 } from "../../../utils/constants.js";
-import * as fs from "fs";
-import * as path from "path";
 
 export class Fetch {
-    public fileName: string;
-    protected remoteAdress: string;
-    protected rawData: any;
+  public fileName: string;
+  protected rawData: any;
 
   /**
    * Create a fetcher.
    * @param {string} fileName - Store base name of remote file.
-   * @const {string} remoteAdress - remote https adress stored in constant.ts file
    * @const {any} rawData - row data downloaded from remote website
    */
 
   constructor() {
     this.fileName = null;
-    this.remoteAdress = REMOTE_DATA_SOURCE_URL;
     this.rawData = null;
   }
 
-  error(message: string): never {
-    throw new Error(message);
-  }
-
-  /** 
+  /**
    * Test remote address and save fetched data
-   * @return {void} 
+   * @return {void}
    */
 
   async download() {
-    try {
-      const response: any = await axios({
-        url: REMOTE_DATA_SOURCE_URL,
-        method: "get",
-        responseType: "stream",
-      });
-      if (response.status == "200") {
-        this.fileName = path.basename(response.request.path);
-        response.data.pipe(
-          fs.createWriteStream(LOCAL_FILES_PATHS.input + this.fileName)
-        ); 
-      } else {
-        this.error("Connection failed");
-      }
-    } catch (error) {
-      this.error(error);
+    const response: any = await axios({
+      url: REMOTE_DATA_SOURCE_URL,
+      method: "get",
+      responseType: "stream",
+    });
+
+    if (response.status == "200") {
+      this.fileName = path.basename(response.request.path);
+      await response.data.pipe(
+        fs.createWriteStream(`${LOCAL_FILES_PATHS.input}${this.fileName}`)
+      );
+    } else {
+      throw new Error("Connection failed");
     }
   }
 }
