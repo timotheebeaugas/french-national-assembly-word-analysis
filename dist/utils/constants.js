4a1325d9e788588badc53666d47212b3892c05d1
Your Name <you@example.com>
2023-02-28 12:52:25 +0100

    Remove ignored files

--- dist/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const LOCAL_FILES_PATHS = {
-    input: "./tmp/",
-    output: "./tmp/",
-};
-export const FETCH_URL_INTERVAL = "0 0 * * *";
-export const REMOTE_DATA_SOURCE_URL = "https://assemblee-nationale.fr/";
