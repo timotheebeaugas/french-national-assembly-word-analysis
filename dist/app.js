cb6fb49b099f5864ea76636ce9cc7d70642a2008
Your Name <you@example.com>
2023-02-28 12:52:25 +0100

    Remove ignored files

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config({ path: './config/.env' });
-import { ParserXML } from "./src/services/Parser/ParserXML.js";
-const test = new ParserXML("unzippedPublicDebateReports");
-test.readFile();
-test.parse();
-test.saveData();
-console.log(test.parsedData);
--- dist/components/cron.js
+++
@@ -1,3 +0,0 @@
-const CronJob = require('cron').CronJob;
-let job = new CronJob("0 0 * * *", function () {
-}, null, true, "Europe/Paris");
