54889c9a5e60361b42c3b89d290ca6295d17a0bc
Your Name <you@example.com>
2023-03-01 15:15:43 +0100

    .

--- app.ts
+++ app.ts
@@ -41,5 +41,5 @@ console.log(test.parsedData)
 
 import { Unzipper } from "./src/services/Unzipper/Unzip.js";
 const test = new Unzipper("publicDebateReports.zip")
-console.log(test)
 console.log(test.unzipOneFile())
+console.log(test)
