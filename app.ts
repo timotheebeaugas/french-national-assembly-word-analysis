9f8b4723c3627b84fccbe41e5f77da0450990b7f
Your Name <you@example.com>
2023-03-11 15:04:40 +0100

    .

--- app.ts
+++ app.ts
@@ -1,11 +1,31 @@
-/** First of all.
- * Import configuration files. 
-*/
+/* import axios from "axios";
 
-import dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config({path: './config/.env'})
+const LOCALFILE: localFile = {
+  input: "./tmp/publicDebateReports.zip",
+  output: "./tmp/unzippedPublicDebateReports.xml"
+}
 
-import { Unzipper } from "./src/services/Unzipper/Unzip.js";
-const test = new Unzipper()
+const downloadZipFile = new Promise((resolve, reject) => {
+  axios({url: URL.value, method: 'get', responseType: 'stream'})
+  .then(function (response) {
+    resolve(response.data)
+  })
+  .catch(function (error) {
+    reject(error) 
+  })
+});
 
-test.unzipOneFile("publicDebateReports.zip")
+/* downloadZipFile
+  .then((data: any)=>{
+    data.pipe(fs.createWriteStream(LOCALFILE.input));  
+  }); */
+
+/* import { LOCAL_FILES_PATHS } from "./utils/env"
+console.log(LOCAL_FILES_PATHS) */
+
+import { ParserXML } from "./src/services/Parser/ParserXML.js";
+
+const test = new ParserXML("unzippedPublicDebateReports");
+test.readFile()
+test.parse()
+test.saveData()
