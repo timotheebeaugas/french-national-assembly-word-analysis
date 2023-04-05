8029fb07201d90f5f1c7c7867381a7897a3ec8ae
Your Name <you@example.com>
2023-04-05 15:27:51 +0200

    revert changes

---
+++ src/services/Database/Connection.ts
@@ -0,0 +1,11 @@
+import { Sequelize } from 'sequelize';
+import * as path  from 'path';
+
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config({path: './config/.env'})
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  });
+
