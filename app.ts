9ac9e2ce381297a3e830968bccc795345224b73d
Your Name <you@example.com>
2023-04-12 15:33:37 +0200

    improve app.js for doing better test during current task

--- app.ts
+++ app.ts
@@ -1,18 +1,31 @@
+import * as fs from "fs";
+import * as path from "path";
 
-// DOWNLOAD DATA
-/* import { Fetch } from "./src/services/Fetch/Fetch.js";
-
-const report = new Fetch("https://www.assemblee-nationale.fr/dyn/opendata/CRSANR5L16S2023O1N205.xml");
-
-report.download();  
-*/
-// PARSING
 import { ParserXML } from "./src/services/Parser/ParserXML.js";
+import { Fetch } from "./src/services/Fetch/Fetch.js";
+import { ReadReport } from "./src/services/Read/ReadReport.js";
 
-const report = new ParserXML("CRSANR5L16S2023O1N205");
+const CURRENT_FILE_ADRESS: string =
+  "https://www.assemblee-nationale.fr/dyn/opendata/CRSANR5L16S2023O1N205.xml";
+const CURRENT_FILENAME: string = path.basename(CURRENT_FILE_ADRESS);
 
-const parsedReport = report.parse();
+// DOWNLOAD DATA IF THEY DOESN'T EXIST LOCALLY
 
-console.log(parsedReport)
+try {
+  const data = fs.existsSync(`tmp/${CURRENT_FILENAME}`);
+  console.log(data);
+  if (data) {
+    // PARSING
+    const report = new ParserXML("CRSANR5L16S2023O1N205");
+    const parsedReport = report.parse();
 
-// d'abord un report 
+    // SAVING IN LOCAL DB
+    console.log(parsedReport);
+
+  } else {
+    const report = new Fetch(CURRENT_FILE_ADRESS);
+    report.download();
+  }
+} catch (err) {
+  console.log(err);
+}
