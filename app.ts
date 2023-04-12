2a3b3ddc7c0dec9870825fa17140c4da451bef26
Your Name <you@example.com>
2023-04-12 15:46:42 +0200

    comments

--- app.ts
+++ app.ts
@@ -5,27 +5,29 @@ import { ParserXML } from "./src/services/Parser/ParserXML.js";
 import { Fetch } from "./src/services/Fetch/Fetch.js";
 import { ReadReport } from "./src/services/Read/ReadReport.js";
 
+// CONST FOR TEST THE CODE WITH ONLY ONE REPORT
 const CURRENT_FILE_ADRESS: string =
   "https://www.assemblee-nationale.fr/dyn/opendata/CRSANR5L16S2023O1N205.xml";
 const CURRENT_FILENAME: string = path.basename(CURRENT_FILE_ADRESS);
 
-// DOWNLOAD DATA IF THEY DOESN'T EXIST LOCALLY
-
+// JOB
 try {
   const data = fs.existsSync(`tmp/${CURRENT_FILENAME}`);
-  console.log(data);
+  // IF REPORT HAS BEEN DOWNLOADED LOCALLY
   if (data) {
     // PARSING
     const report = new ParserXML("CRSANR5L16S2023O1N205");
     const parsedReport = report.parse();
 
-    // SAVING IN LOCAL DB
+    // SAVING DATA IN LOCAL DB
     console.log(parsedReport);
 
   } else {
+    // DOWNLOAD THE REPORT BY REMOTE URL
     const report = new Fetch(CURRENT_FILE_ADRESS);
     report.download();
   }
 } catch (err) {
+    // PRINT ERR(S)
   console.log(err);
 }
