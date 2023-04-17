92b9fe144bc236ecbff6a97fd196f7a316f12d3a
Your Name <you@example.com>
2023-04-17 13:29:26 +0200

    fix some problems with typeorm

--- src/services/Database/Connection.ts
+++ src/services/Database/Connection.ts
@@ -1,7 +1,16 @@
 import "reflect-metadata";
+import * as dotenv from 'dotenv';
+
 import { DataSource } from "typeorm"
 import { Report } from "../../models/entities/Report.entity.js";
-import * as dotenv from 'dotenv';
+import {Mandate} from "../../models/entities/Mandate.entity.js";
+import { Speech } from "../../models/entities/Speech.entity.js";
+import { AgendaItem } from "../../models/entities/AgendaItem.entity.js";
+import { PoliticalBody } from "../../models/entities/PoliticalBody.entity.js";
+import { Constituency } from "../../models/entities/Constituency.entity.js";
+import { Actor } from "../../models/entities/Actor.entity.js";
+
+
 dotenv.config(); 
 
 export const AppDataSource = new DataSource({
@@ -12,7 +21,7 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   schema: process.env.DB_SCHEMA,
-  entities: [`../../models/entities/*.entity.{js,ts}`], 
+  entities: [ Mandate, AgendaItem, Actor, Speech, Report, PoliticalBody, Constituency], //[`../../models/entities/*.entity.{js,ts}`] don't works
   synchronize: true,
   logging: true,
 });
